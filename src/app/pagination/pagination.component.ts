1c7e99d88f8592499b39498feeca3c8060e552f2
aninaslyan <ani.naslyan@gmail.com>
2020-01-07 21:43:55 +0400

    finished pagination functionality

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { Event } from '../events-layout/event.model';
@@ -10,13 +10,15 @@ import { EventService } from '../events-layout/event.service';
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnChanges {
+export class PaginationComponent implements OnChanges, OnInit {
   @Input() totalCount: number;
   @Input() limit: number;
   events: Event[];
   pageNums: Array<number>;
+  currentPage: number;
   eventsSubscription: Subscription;
 
+  // todo make this component fully reusable
   constructor(private paginationService: PaginationService, private eventService: EventService) {
   }
 
@@ -25,6 +27,8 @@ export class PaginationComponent implements OnChanges {
   }
 
   onPageNum(num: number) {
+    this.currentPage = num;
+
     this.eventService.fetchEventsAndTypes(num, this.limit)
         .subscribe((response) => {
           this.events = this.eventService.getEventTypeFromNumber(response);
@@ -36,5 +40,21 @@ export class PaginationComponent implements OnChanges {
         });
 
     this.events = this.eventService.getEvents();
+  }
+
+  ngOnInit() {
+    this.currentPage = 1;
+  }
+
+  onPrevClick() {
+    if (this.currentPage - 1 >= 1) {
+      this.onPageNum(this.currentPage - 1);
+    }
+  }
+
+  onNextClick() {
+    if (this.currentPage < this.pageNums.length) {
+      this.onPageNum(this.currentPage + 1);
+    }
   }
 }
