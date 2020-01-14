b64dbe955ecadb33a67fd9814165aa6db1cfe8fa
aninaslyan <ani.naslyan@gmail.com>
2020-01-14 14:34:46 +0400

    adding styles related to form and header

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -6,8 +6,7 @@ import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
   signInForm: FormGroup;
